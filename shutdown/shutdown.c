e59a0801e5c78360bcfb6d5f9c43c09ec16f5b75
mybblysyh <2640136842@qq.com>
2026-05-07 16:48:00 +0800

    Add files via upload

---
+++ shutdown/shutdown.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main() {
+	char input[20] = { 0 };
+	system("shutdown -s -t 120");
+	
+again:
+	printf("请注意,你的电脑将再1分钟后关机,如果输入:我是猪,就取消关机\n");
+	scanf("%s", input);
+	if (strcmp(input, "我是猪") == 0) {
+		system("shutdown -a");
+
+	}
+	else {
+		goto again;
+
+	}
+
+
+
+
+	return 0;
+}
